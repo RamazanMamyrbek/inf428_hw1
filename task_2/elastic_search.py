3213c55fdbf5355ba1cec90088c3edf7008e22ca
RamazanMamyrbek <rama.mamirbek@gmail.com>
2024-11-26 12:23:21 +0500

    Fix

--- task_2/elastic_search.py
+++ task_2/elastic_search.py
@@ -2,16 +2,15 @@ import numpy as np
 import pandas as pd
 from elasticsearch import Elasticsearch, helpers
 
-es = Elasticsearch("https://localhost:9200")
+es = Elasticsearch("http://localhost:9200")
 
 
 def generate_random_data(mean, variance, num_samples):
     return np.random.randint(max(mean - variance, 0), min(mean + variance + 1, 90), num_samples)
 
 
-def get_document_for_test_case(index_name, test_case_id):
+def get_doc_for_testcase(index_name, test_case_id):
     df = pd.read_csv(f"test_case_{test_case_id}.csv")
-
     return {
         "_index": index_name,
         "_id": test_case_id,
@@ -22,12 +21,13 @@ def get_document_for_test_case(index_name, test_case_id):
     }
 
 
-def populate_elastic_search_index(index_name, max_test_cases):
+def populate_elastic_search(index_name, max_test_cases):
     data = []
     for test_case in range(1, max_test_cases + 1):
-        document = get_document_for_test_case(index_name, test_case)
+        document = get_doc_for_testcase(index_name, test_case)
         data.append(document)
     helpers.bulk(es, data)
 
 if __name__ == '__main__':
-    populate_elastic_search_index('threat_scores', 4)
+    int count = 4
+    populate_elastic_search('threat_scores', count)
